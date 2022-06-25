801a644d2c87c181ba6ab5e0485c0a19fa1a0f85
Edwin Ortiz <edarcode@gmail.com>
2022-06-25 08:25:34 -0500

    init proyect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { dashboard, home } from "./constants/paths";
+import Admin from "./views/Admin/Admin";
+import Client from "./views/Client/Client";
+import Dashboard from "./views/Dashboard/Dashboard";
+import Home from "./views/Home/Home";
+import NotFound from "./views/NotFound/NotFound";
 
 function App() {
-	return <div className="App">edarcode</div>;
+	return (
+		<div className="App">
+			<Routes>
+				{/* ------------------CLIENT------------------------------ */}
+				<Route path={home} element={<Client />}>
+					<Route index element={<Home />} />
+				</Route>
+				{/* ------------------ADMIN------------------------------ */}
+				<Route path={dashboard} element={<Admin />}>
+					<Route index element={<Dashboard />} />
+				</Route>
+				{/* ------------------NOT FOUND------------------------------ */}
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/views/Client/Client.jsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router-dom";
+
+export default function Client() {
+	return (
+		<div>
+			<Outlet />
+		</div>
+	);
+}
